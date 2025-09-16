00a061107ab2f493edf36400d6a61e3b9394b0c6
youness boumeshouli <boumeshouliyouness6@gmail.com>
2025-09-16 16:16:57 +0100

    finishing project

---
+++ PythonProject/function.py
@@ -0,0 +1,25 @@
+# Challenge 1 : Présentation personnalisé
+
+# nom = input("entrer votre nom    ")
+# salaire = int(input("entrer votre salaire "))
+# nh = int(input("entrer votre nombre d'heures "))
+# salaire_total = 0
+# def horaire_sup(nom,salaire,nh):
+#     if nh > 40:
+#         plus = nh - 40
+#         salaire_nor = 40 * salaire
+#         salaire_supp: float = plus * 1.5 * salaire
+#         salaire_total = salaire_nor + salaire_supp
+#     else:
+#         salaire_total = nh * salaire
+#
+#     print("le salairie ", nom, "  a :", salaire_total)
+# horaire_sup(nom,salaire,nh)
+
+# Challenge  2 : Fonction calculation() – somme et différence
+
+def calculation(num1,num2):
+    print(num1+num2)
+    print(num1-num2)
+
+calculation(12,2)
