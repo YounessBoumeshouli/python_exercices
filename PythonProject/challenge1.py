5463725fe8e39fc7924126f1dfa7b98a80ab3e99
youness boumeshouli <boumeshouliyouness6@gmail.com>
2025-09-16 11:48:20 +0100

    complete challenge 7

--- PythonProject/challenge1.py
+++ PythonProject/challenge1.py
@@ -1,3 +1,99 @@
-prenom  = input("entrer votre nom    ")
-age  = int(input("entrer votre age "))
-print(prenom + "  " + age)
+# challenge 1
+# prenom  = input("entrer votre nom    ")
+# age  = int(input("entrer votre age "))
+# print(prenom + "  " + age)
+import math
+
+# CHALLEGE2
+
+
+# nom = input("entrer votre nom    ")
+# salaire = int(input("entrer votre salaire "))
+# nh = int(input("entrer votre nombre d'heures "))
+# salaire_total = 0
+# if nh > 40:
+#     plus = nh - 40
+#     salaire_nor = 40 * salaire
+#     salaire_supp : float  = plus * 1.5 * salaire
+#     salaire_total = salaire_nor + salaire_supp
+# else :
+#     salaire_total = nh * salaire
+#
+# print("le salairie " , nom , "  a :",salaire_total )
+
+# CHALLENGE 3
+# salaire = 0
+# nh = 0
+# nom = ""
+#
+#
+# nom = input("entrer votre nom    ")
+# try:
+#         salaire = int(input("entrer votre salaire "))
+#         try:
+#             nh = int(input("entrer votre nombre d'heures "))
+#             salaire_total = 0
+#             if nh > 40:
+#                 plus = nh - 40
+#                 salaire_nor = 40 * salaire
+#                 salaire_supp: float = plus * 1.5 * salaire
+#                 salaire_total = salaire_nor + salaire_supp
+#             else:
+#                 salaire_total = nh * salaire
+#
+#             print("le salairie ", nom, "  a :", salaire_total)
+#         except ValueError as error:
+#             print("the input must be  a  number", error)
+# except ValueError as error:
+#         print("the input must be  a  number", error)
+
+# print("the input must be  a  String")
+
+
+# Challenge 4
+# num1 = float(input("entrer le nombre 1"))
+# num2 = float(input("entrer le nombre 2"))
+# if num1 * num2 >= 0:
+#     if num1 == 0 or num2 == 0 :
+#         print("est zero")
+#     else :
+#         print("est positif")
+#
+#
+# else :
+#     print("est negative")
+
+# Challenge 5
+
+# n =  int(input("number"))
+# i = 0
+# s = 0
+# if(n>0):
+#
+#     while i <= n:
+#      s += i
+#      print(s)
+#      i += 1
+
+# Challenge 6 : Inverser une chaîne
+
+# str =   input("enter a name")
+# length = len(str)
+# i = 0
+# newString = ""
+# while length >= 1 :
+#     newString += str[length-1]
+#     length-= 1
+#
+# print(newString)
+
+# Challenge 7
+# x1 = int(input("x1"))
+# y1 = int(input("y1"))
+# x2 = int(input("x2"))
+# y2 = int(input("y2"))
+# print(math.sqrt((x2-x1)**2 + (y2-y1)**2))
+
+
+
+
